000f6c4d300c654b08ca56de5660ded7a3aac0dd
Won A Young <zeronediary@naver.com>
2023-11-08 11:44:32 +0900

    useEffect 연습 고객 번호들 조회하기 까지

---
+++ src/main/java/com/example/spring20230920/controller/MyDao10.java
@@ -0,0 +1,41 @@
+package com.example.spring20230920.controller;
+
+import com.example.spring20230920.domain.MyDto33Employee;
+import com.example.spring20230920.domain.MyDto34Customer;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value;
+
+import java.util.List;
+
+@Mapper
+public interface MyDao10 {
+    @Select("""
+    SELECT CustomerName FROM customers
+    WHERE CustomerID = #{id}
+    """)
+    String select(Integer id);
+
+    @Select("""
+    SELECT * FROM customers
+    WHERE CustomerID=50
+    """)
+    MyDto34Customer getCustomer();
+
+    @Select("""
+    SELECT * FROM customers
+    WHERE CustomerID = #{id}
+    """)
+    MyDto34Customer getCustomerById(Integer id);
+
+    @Select("""
+    SELECT * FROM employees
+    WHERE EmployeeID = #{id}
+    """)
+    MyDto33Employee getEmployeeById(Integer id);
+
+    @Select("""
+    SELECT customerId FROM customers ORDER BY CustomerID
+    """)
+    List<Integer> getCustomerIdList();
+}
---
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -0,0 +1,66 @@
+package com.example.spring20230920.controller;
+
+import com.example.spring20230920.domain.MyDto33Employee;
+import com.example.spring20230920.domain.MyDto34Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("api/main1")
+public class RestController1 {
+    private final MyDao10 dao;
+
+    @GetMapping("sub1")
+    @ResponseBody
+    public String method1(){
+        return "hello boot app!!";
+    }
+
+    @GetMapping("sub2")
+    @ResponseBody
+    public String method2(){
+        //5번 고객의 customerName 응답
+        return dao.select(6);
+    }
+
+    @GetMapping("sub3")
+    @ResponseBody
+    public MyDto34Customer method3(){
+        return dao.getCustomer();
+    }
+
+
+    // /api/main1/sub4?id=33
+    @GetMapping("sub4")
+    @ResponseBody
+    public ResponseEntity<MyDto34Customer> method4(Integer id){
+        MyDto34Customer customer = dao.getCustomerById(id);
+        if(customer==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(customer);
+    }
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public ResponseEntity<MyDto33Employee> method5(Integer id){
+        MyDto33Employee employee = dao.getEmployeeById(id);
+        if(employee==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping("sub6")
+    @ResponseBody
+    public List<Integer> method6(){
+        return dao.getCustomerIdList();
+    }
+}
